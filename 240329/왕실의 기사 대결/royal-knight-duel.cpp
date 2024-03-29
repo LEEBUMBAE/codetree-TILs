a2f9d4a042fed406c26dd8b7d64593ec22a73456
LEEBUMBAE <122437229+LEEBUMBAE@users.noreply.github.com>
2024-03-29 14:47:47 +0900

    Title: 왕실의 기사 대결, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240329/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240329/왕실의 기사 대결/royal-knight-duel.cpp
@@ -92,11 +92,6 @@ int main() {
 }
 
 // 1 기사이동
-// 상하좌우 한칸 이동
-// 이동하려는칸에 다른 기사 존재시 그 기사도 함께 연쇄적으로 한칸 밀림
-// 그 옆또 기사 있으면 또 밀림
-// 이동방향 끝에 벽이 있다면 모든 기사는 이동 불가
-// 체스판 밖 기사에게 명령하면 아무 반응없음
 bool canMove(int knightN, int knightD, int cnt) {
 	Knight &knight = knightList[knightN];
 	// 기사가 죽은놈인지 판별(체스판 밖 기사면 안됨)
@@ -132,12 +127,6 @@ bool canMove(int knightN, int knightD, int cnt) {
 
 pair<int, vector<int>> search(Knight& knight, int knightD) {
 	// search의 결과 {1, 0}=벽 {2, 기사번호}=기사 {3, 0}=장애물or빈칸
-	// 조사를 함
-	// map을 조사함
-	// map에서 벽(2)이있으면 그냥 {1, 0} return
-	// map에서 벽없이(0 or 1) -> 기사맵 확인후 기사발견하면 isKnight = 기사번호
-	// map에서 끝까지 장애물 or 빈칸만 있으면(마지막에 isKnight == 0 면) {3, 0} return
-	// 아근데 => 기사가 여러명일수있네 ;; vector<int> isKnight 배열에 기사 있으면 1저장 아니면 0저장하자..
 	vector<int> isKnight(N + 1, 0);
 	vector<int> wallOrEmpty;
 	if (knightD == 0) { // 상
@@ -220,7 +209,6 @@ pair<int, vector<int>> search(Knight& knight, int knightD) {
 				}
 			}
 		}
-		// map에서 끝까지 장애물 or 빈칸만 있으면(마지막에 isKnight.size() == 0 면) {3, wallOrEmpty} return
 		for (int i = 1; i <= N; i++) {
 			if (isKnight[i] == 1) {
 				return { 2, isKnight };
@@ -231,8 +219,6 @@ pair<int, vector<int>> search(Knight& knight, int knightD) {
 }
 
 void move(int knightN, int knightD) {
-	// 움직일땐 명령받은 덩어리 기준 통째로 옮기면됨 <= 잘못된생각
-	// => 덩어리를 bfs로 그냥 찾는게 아니라  명령받은애랑, attacked 당한애만 bfs 돌면서 움직여야함
 	
 	vector<vector<int>> tempMap(L + 1, vector<int>(L + 1));
 	vector<vector<bool>> visited(L + 1, vector<bool>(L + 1));
@@ -240,8 +226,7 @@ void move(int knightN, int knightD) {
 
 	q.push({ knightList[knightN].r , knightList[knightN].c });
 	visited[knightList[knightN].r][knightList[knightN].c] = true;
-	// 이동하기
-	// 순회할때는 knightMap 이용해서 덩어리 기존위치를 기준으로 순회 
+
 	while (!q.empty()) {
 		int curR = q.front().first;
 		int curC = q.front().second;
@@ -288,20 +273,7 @@ void move(int knightN, int knightD) {
 }
 
 // 2 대결 대미지
-// 명령받은 기사가 다른 기사를 밀치면 밀려난 기사들은 피해 입음
-// 이동완료한 곳에 w x h 직사각형 내에 있는 함정의 수만큼 피해입음
-// 받은 피해만큼 체력 감소, 현재 체력이상의 대미지 받으면 체스판에서 사라짐
-// 단 명령을 받은 기사는 피해 안 입음
-// 기사들은 모두 밀린 이후 대미지 입음
-// 밀렸더라도 함정이 없으면 피해는 없는것임
 void getDamage() {
-	// 기사배열순회
-		// 첫명령 기사면 피해 안입음
-		// 밀쳐진 기사면 피해입음 
-		// 밀쳐진 기사면 해당범위 map 조사 해서(bfs로 해당기사 번호 기사맵 조사하면됨) 장애물수만큼 체력 감소
-		// 입은 대미지 기록
-		// 이번턴에 죽은기사는 기사맵에서 지우고, 기사정보에 죽었다고 기록
-		// 명령받은, 밀쳐진기사여부 초기화
 	for (int i = 1; i <= N; i++) {
 		Knight &curKnight = knightList[i];
 		
@@ -330,14 +302,11 @@ void getDamage() {
 				}
 			}
 
-			// 장애물수만큼 체력 감소 및 대미지 누적
 			curKnight.k -= trapNum;
 			curKnight.damageSum += trapNum;
 
-			// 명령받은, 밀쳐진기사여부 초기화
 			curKnight.attacked = false;
 
-			// 이번턴에 죽은기사는 기사맵에서 지우고, 기사정보에 죽었다고 기록
 			if (curKnight.k <= 0) {
 				for (int i = 1; i <= L; i++) {
 					for (int j = 1; j <= L; j++) {
@@ -353,7 +322,6 @@ void getDamage() {
 					int curC = q.front().second;
 					q.pop();
 
-					// 방문한 곳은 기사맵에서 지워버려
 					knightMap[curR][curC] = 0;
 
 					for (int d = 0; d < 4; d++) {
